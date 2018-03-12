c87aa2dc97a308457a3afdac78eb61e3304f49b1
malyushkin <malyushkinr@gmail.com>
2018-03-12 10:39:55 +0300

    helpdesk video render & styling

--- front/views/default/index.php
+++ front/views/default/index.php
@@ -1,3 +1,9 @@
+<?php
+
+use rmrevin\yii\fontawesome\FA;
+
+?>
+
 <div class="front-default-index">
     <div class="front-banners">
         1
@@ -12,16 +18,28 @@
         <div class="row">
             <div class="col-md-6">
                 <div class="video">
-
+                    <div class="message align-middle">
+                        <div class="title text-center">Как тут все работает?</div>
+                        <div class="icon text-center">
+                            <a href="#">
+                                <div class="play-icon">
+                                    <?= FA::i('play'); ?>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="title text-center">Посмотрите обучающее видео</div>
+                    </div>
                 </div>
             </div>
             <div class="col-md-6">
-                <div class="row">
-                    <div class="col-md-6">
+                <div class="order">
+                    <div class="row">
+                        <div class="col-md-6">
 
-                    </div>
-                    <div class="col-md-6">
-                        Отследить
+                        </div>
+                        <div class="col-md-6">
+                            Отследить
+                        </div>
                     </div>
                 </div>
             </div>

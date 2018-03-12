8abe50eecc6f9b4c7f205d4aa071f7487d66bb58
malyushkin <malyushkinr@gmail.com>
2018-03-12 10:11:28 +0300

    partners render & styling

--- front/views/default/index.php
+++ front/views/default/index.php
@@ -1,12 +1,52 @@
 <div class="front-default-index">
     <div class="front-banners">
-1
+        1
     </div>
     <div class="fornt-hot">
-2
+        2
     </div>
     <div class="front-search">
-3
+        3
     </div>
+    <div class="front-helpdesk">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="video">
 
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-6">
+
+                    </div>
+                    <div class="col-md-6">
+                        Отследить
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="front-partners">
+        <div class="row">
+            <div class="col-md-2">
+                <div class="partner"></div>
+            </div>
+            <div class="col-md-2">
+                <div class="partner"></div>
+            </div>
+            <div class="col-md-2">
+                <div class="partner"></div>
+            </div>
+            <div class="col-md-2">
+                <div class="partner"></div>
+            </div>
+            <div class="col-md-2">
+                <div class="partner"></div>
+            </div>
+            <div class="col-md-2">
+                <div class="partner"></div>
+            </div>
+        </div>
+    </div>
 </div>

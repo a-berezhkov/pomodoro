c62959d532a6c68891330f2e3648bdbd80e5ab39
malyushkin <malyushkinr@gmail.com>
2018-03-20 19:00:56 +0300

    auth & reg modal layouts

--- widgets/views/login.php
+++ widgets/views/login.php
@@ -7,12 +7,12 @@ use app\widgets\CustomModal;
 
 $script = <<< JS
     $( "#registration" ).click(function() {
-        $( "#auth" ).hide( "slow" );
-        $( "#reg" ).show( "slow" );
+        $( "#auth" ).slideUp( "slow" );
+        $( "#reg" ).slideDown( "slow" );
     });
     $( "#authorization" ).click(function() {
-        $( "#reg" ).hide( "slow" );
-        $( "#auth" ).show( "slow" );
+        $( "#reg" ).slideUp( "slow" );
+        $( "#auth" ).slideDown( "slow" );
     });
 JS;
 $this->registerJs($script, yii\web\View::POS_READY)
@@ -37,24 +37,60 @@ CustomModal::begin([
             <h1 class="title">Вход</h1>
 
             <div class="content">
-            <?php $form = ActiveForm::begin([
-                'id' => 'login-form',
-                'enableAjaxValidation' => true,
-                'action' => ['/user/login']
+                <?php $form = ActiveForm::begin([
+                    'id' => 'login-form',
+                    'enableAjaxValidation' => true,
+                    'action' => ['/user/login']
 
-            ]);
-            ?>
+                ]);
+                ?>
                 <?php
-            echo $form->field($model, 'login')->textInput(['placeholder' => $model->getAttributeLabel('login')])->label(false);
-            echo $form->field($model, 'password')->passwordInput(['placeholder' => $model->getAttributeLabel('password')])->label(false);
-            ?>
+                echo $form->field($model, 'login')->textInput(['placeholder' => $model->getAttributeLabel('email')])->label(false);
+                echo $form->field($model, 'password')->passwordInput(['placeholder' => $model->getAttributeLabel('password')])->label(false);
+                ?>
             </div>
 
             <div class="form-group text-right">
                 <?= Html::submitButton(
                     'Войти',
-                    ['class' => 'btn button button-enter', 'tabindex' => '4']
+                    ['class' => 'btn button button-action', 'tabindex' => '4']
                 ) ?>
+            </div>
+
+            <?php ActiveForm::end(); ?>
+        </div>
+
+        <div class="col-md-4 sub">
+            <h2 class="title">Вы у нас в первый раз?</h2>
+            <div class="content">
+                <p>Зарегистрируйтесь на нашем сайте и получите уникальную возможность делать оптовые закупки для своей компании.</p>
+            </div>
+            <? echo Html::a('Регистрация', null, ['class' => 'btn button-bordered', 'name' => 'Registration-button', 'id' => 'registration']); ?>
+        </div>
+
+    </div>
+    <!------------------------------------Регситрация -------------------------------------------------------------->
+    <div id="reg">
+        <div class="col-md-8 main">
+
+            <h1 class="title">Регистрация</h1>
+
+            <div class="content">
+                <?php $form = ActiveForm::begin([
+                    'id' => 'registration-form',
+                    'enableAjaxValidation' => true,
+                    'action' => ['/user/register']
+
+                ]); ?>
+
+                <?= $form->field($modelRegistrationForm, 'email')->textInput(['placeholder' => $model->getAttributeLabel('email')])->label(false) ?>
+                <?//= $form->field($modelRegistrationForm, 'username')->label(false) ?>
+                <?= $form->field($modelRegistrationForm, 'password')->passwordInput(['placeholder' => $model->getAttributeLabel('password')])->label(false) ?>
+
+            </div>
+
+            <div class="form-group text-right">
+                <?= Html::submitButton(Yii::t('user', 'Sign up'), ['class' => 'btn button button-action']) ?>
             </div>
 
             <?php ActiveForm::end(); ?>
@@ -65,34 +101,8 @@ CustomModal::begin([
             <div class="content">
                 <p>А может Вы уже зарегистрировались на нашем портале? Тогда смелее нажимайте на кнопку «войти».</p>
             </div>
-            <? echo Html::a('Регистрация', null, ['class' => 'btn button-bordered', 'name' => 'Registration-button', 'id' => 'registration']); ?>
-        </div>
 
-    </div>
-    <!------------------------------------Регситрация -------------------------------------------------------------->
-    <div id="reg" style="display: none">
-        <div class="col-md-8 main">
-
-            <?php $form = ActiveForm::begin([
-                'id' => 'registration-form',
-                'enableAjaxValidation' => true,
-                'action' => ['/user/register']
-
-            ]); ?>
-
-            <?= $form->field($modelRegistrationForm, 'email') ?>
-            <?= $form->field($modelRegistrationForm, 'username') ?>
-            <?= $form->field($modelRegistrationForm, 'password')->passwordInput() ?>
-
-            <div class="form-group">
-                <?= Html::submitButton(Yii::t('user', 'Sign up'), ['class' => 'btn btn-success btn-block']) ?>
-            </div>
-
-            <?php ActiveForm::end(); ?>
-        </div>
-
-        <div class="col-md-4 sub">
-            <? echo Html::a('Войти', null, ['class' => 'btn btn-primary', 'name' => 'Registration-button', 'id' => 'authorization']); ?>
+            <? echo Html::a('Войти', null, ['class' => 'btn button-bordered input-block-level', 'name' => 'Registration-button', 'id' => 'authorization']); ?>
         </div>
     </div>
 

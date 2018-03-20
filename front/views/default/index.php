164aea9bc1d41095ddd03347c376cc90b50eb508
Andrei V. Berezhkov <dead0343@gmail.com>
2018-03-20 16:43:32 +0300

    make slider without data

--- front/views/default/index.php
+++ front/views/default/index.php
@@ -6,32 +6,61 @@ use yii\helpers\Html;
 /**
  * @var   $suggests \app\front\models\Store
  */
-?>
 
+?>
+<!-- TODO перенести в SCSS стили для слайдера -->
+<style>
+
+    .carousel-control.right, .carousel-control.left {
+        background-image: none;
+    }
+
+    .carousel-control {
+        color: #0f0f0f ;
+    }
+
+    .front-banners .glyphicon-chevron-right,.front-banners .glyphicon-chevron-left   {
+        /* Фон значков и цвет*/
+        color: white;
+        background: black;
+        border-radius: 50%;
+        padding: 5px;
+        border: 2px solid white;
+    }
+
+    .carousel-control .glyphicon-chevron-left, .carousel-control .glyphicon-chevron-right, .carousel-control .icon-prev, .carousel-control .icon-next {
+        /*Что бы значки были круглыми */
+        width: auto;
+        height: auto;
+    }
+
+    .carousel-control .glyphicon-chevron-right {
+        right: 25%;
+    }
+    .carousel-control .glyphicon-chevron-left {
+        left: 25%;
+    }
+
+
+</style>
 <div class="front-default-index">
     <div class="front-banners">
         <div class="row">
+
             <div class="col-md-8">
-                <div class="product main">
-                    <?= Html::img(['/img/products/m-1.png']); ?>
-                    <div class="details">
-                        <div class="name text-center">
-                            Томаты Бакинские
-                        </div>
-                        <div class="price text-center">
-                            <s>7550</s> 6850 ₽
-                        </div>
-                        <div class="description text-center">
-                            Цена за упаковку
-                            <div class="meta">
-                                <span class="weight">25</span>
-                                <span class="boxes">12</span>
-                            </div>
-                        </div>
-                        <button class="btn button button-more">Подробнее</button>
-                    </div>
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                    <?= $this->render('_slider'); ?>
                 </div>
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
+
             <div class="col-md-4">
                 <?=
                 /**
@@ -40,8 +69,8 @@ use yii\helpers\Html;
                  */
                 $this->render('_categories-right', ['categories' => $categories])
                 ?>
-
             </div>
+
         </div>
         <?=
         /**
@@ -54,15 +83,17 @@ use yii\helpers\Html;
     </div>
     <div class="front-hot">
         <h2 class="title text-center">Горячие предложения</h2>
-        <p class="text-center">Автор неверно акцентирует внимание в своей работе на новину какой-то штуки. В статье
-            представлены расчеты чего-нибудь, которые полностью расходятся с тем, что должно иметь место в соответствии
+        <p class="text-center">Автор неверно акцентирует внимание в своей работе на новину какой-то штуки. В
+            статье
+            представлены расчеты чего-нибудь, которые полностью расходятся с тем, что должно иметь место в
+            соответствии
             с какой-нибудь классической теорией. </p>
         <?=
         /**
          * Раздел c товарами
          * Категории справа
          */
-        $this->render('_suggest', ['suggests' =>  $suggests])
+        $this->render('_suggest', ['suggests' => $suggests])
         ?>
 
     </div>

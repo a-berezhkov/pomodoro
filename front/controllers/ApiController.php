1347aa596781e96801a9ba64883592b6aad0e8d1
Andrei V. Berezhkov <dead0343@gmail.com>
2018-03-26 19:07:28 +0300

    автозаполнение адреса dev

--- front/controllers/ApiController.php
+++ front/controllers/ApiController.php
@@ -10,6 +10,7 @@ namespace app\front\controllers;
 
 
 use app\front\models\Api;
+use app\front\models\user\Profile;
 use yii\web\Controller;
 
 class ApiController extends Controller
@@ -22,5 +23,16 @@ class ApiController extends Controller
     public function actionGetAddressByGoogleMaps( $q ) {
         \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
         return Api::getPlacesByGoogleMap($q);
+    }
+
+    public function actionGetProfile(){
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+        $userId = \Yii::$app->user->id;
+        if ($userId){
+            return  $profile = Profile::find()->where(['user_id'=>$userId])->asArray()->one();
+        } else {
+            return 'Something wrong at API';
+        }
+
     }
 }

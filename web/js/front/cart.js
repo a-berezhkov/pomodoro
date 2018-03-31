3d1f1e194765bb3e563b5b4fe5937d2fd96343c8
Andrei V. Berezhkov <dead0343@gmail.com>
2018-03-31 15:55:08 +0300

    delete items from cart

--- web/js/front/cart.js
+++ web/js/front/cart.js
@@ -2,6 +2,19 @@
  *
  */
 showItems();
+window.onload = function() {
+    $( ".fa-trash" ).click(function() {
+        var item = $(this).attr('data-item');
+         if (confirm("Вы действительно хотите это сделать?")){
+              localStorage.removeItem(item);
+             $(this).parent().parent().hide("slow" );
+             setBadgeBasket();
+             // todo update count and total
+         }
+
+       // alert($(this).attr('data-item'));
+    });
+};
 function showItems() {
     //начальные значения для расчета сумм показателей
     var discountCountBox = 0;
@@ -16,8 +29,8 @@ function showItems() {
     var hot = document.getElementById('hot');
     var hot_div = document.createElement('div');
     hot_div.className = "row";
-    for (var item in localStorage) {
-        item = JSON.parse(localStorage.getItem(item));
+    for (var item_name in localStorage) {
+        item = JSON.parse(localStorage.getItem(item_name));
         console.log(item);
         if (item !== null ) {
             if (parseInt(item.is_sale) === 1){
@@ -28,14 +41,14 @@ function showItems() {
                 // создание элемента для вставки
                 hot_div.innerHTML =  hot_div.innerHTML +
                     '<div class="row">' +
-                    '<div class="col-md-1"><i class="icon-trash" aria-hidden="true"></i></div>' +
+                    '<div class="col-md-1"><i class="fa fa-trash" data-item="'+item_name+'" aria-hidden="true"></i></div>' +
                     '<div class="col-md-2"><img src="' + item.item_image_link + '"' +
                     '  width="100" height="70" alt=""></a></div>' +
                     '<div class="col-md-3">' + item.item_name + '</div>' +
                     '<div class="col-md-2">' + item.count_box + '</div>' +
                     '<div class="col-md-2">' + item.item_box_weight + '</div>' +
                     '<div class="col-md-2"><s>'+item.item_box_price +'</s><br>' + item.item_discount_box_price + '</div>' +
-                    '</div><hr>';
+                    '<hr></div>';
             }
         }
     }
@@ -49,8 +62,8 @@ function showItems() {
     var ordinary = document.getElementById('ordinary');
     var ordinary_div = document.createElement('div');
     ordinary_div.className = "row";
-    for (var item in localStorage) {
-        item = JSON.parse(localStorage.getItem(item));
+    for (var item_name in localStorage) {
+        item = JSON.parse(localStorage.getItem(item_name));
         if (item !== null ) {
             if (parseInt(item.is_sale) === 0){
                 // Расчет показателей
@@ -60,14 +73,14 @@ function showItems() {
                 // создание элемента для вставки
                 ordinary_div.innerHTML =  ordinary_div.innerHTML +
                     '<div class="row">' +
-                    '<div class="col-md-1"><i class="icon-trash" aria-hidden="true"></i></div>' +
+                    '<div class="col-md-1"><i class="fa fa-trash" data-item="'+item_name+'" aria-hidden="true"></i></div>' +
                     '<div class="col-md-2"><img src="' + item.item_image_link + '"' +
                     '  width="100" height="70" alt=""></a></div>' +
                     '<div class="col-md-3">' + item.item_name + '</div>' +
                     '<div class="col-md-2">' + item.count_box + '</div>' +
                     '<div class="col-md-2">' + item.item_box_weight + '</div>' +
                     '<div class="col-md-2">' + item.item_box_price + '</div>' +
-                    '</div><hr>';
+                    '<hr></div>';
             }
         }
     }

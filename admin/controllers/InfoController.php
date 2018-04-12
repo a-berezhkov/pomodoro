ed42096f7372e8e1b98882a2e4a4c0fe12d9b326
Ziablik <Ziablikl@mail.ru>
2018-04-12 16:02:21 +0300

    fix InfoController

--- admin/controllers/InfoController.php
+++ admin/controllers/InfoController.php
@@ -11,13 +11,12 @@ namespace app\admin\controllers;
 
 use app\front\models\OrdersHasCart;
 use app\front\models\user\Profile;
+use yii\data\ActiveDataProvider;
 use yii\data\ArrayDataProvider;
 use yii\filters\VerbFilter;
 use yii\helpers\VarDumper;
 use yii\web\Controller;
-use app\admin\models\aOrders;
 use app\front\models\Cart;
-use app\admin\models\aOrdersStatus;
 use yii\web\NotFoundHttpException;
 
 class InfoController extends Controller
@@ -41,19 +40,19 @@ class InfoController extends Controller
     {
         $cart_id = OrdersHasCart::find()->asArray()->where('order_id='.$id)->one()['cart_id'];
         $profile_id = Cart::find()->asArray()->where('id='.$cart_id)->one()['profile_id'];
-        $cart_list = Cart::find()->asArray()->where('profile_id='.$profile_id)->all();
+        $query = OrdersHasCart::find()
+            ->innerJoin('orders', 'orders.id=order_id')
+            ->innerJoin('cart', 'cart.id=cart_id')
+            ->innerJoin('profile', 'profile.user_id=cart.profile_id')
+            ->innerJoin('orders_status', 'orders_status.id=orders.delivery_status')
+            ->where('cart.profile_id='.$profile_id);
         $profile_name = Profile::find()->asArray()->where('user_id='.$profile_id)->one()['name'];
-        $dataProvider = new ArrayDataProvider([
-            'allModels' => $cart_list
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query
         ]);
-//        VarDumper::dump($data_provider,10,true);
         return $this->render('view',[
             'dataProvider' => $dataProvider,
-            'profile_name' => $profile_name
 
         ]);
-
-//        $model = OrdersHasCart::find()->asArray()->where('order_id='.$id)->one();
-//        VarDumper::dump($cart_id,10,true);
     }
 }

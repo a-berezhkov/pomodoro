d6bfd5ff7340fc005e3416930b2311501a1537f5
Ziablik <Ziablikl@mail.ru>
2018-04-12 23:38:20 +0300

    upd admin\controllers\InfoController

--- admin/controllers/InfoController.php
+++ admin/controllers/InfoController.php
@@ -9,6 +9,7 @@
 namespace app\admin\controllers;
 
 
+use app\admin\models\DateForm;
 use app\front\models\OrdersHasCart;
 use app\front\models\user\Profile;
 use yii\data\ActiveDataProvider;
@@ -18,6 +19,7 @@ use yii\helpers\VarDumper;
 use yii\web\Controller;
 use app\front\models\Cart;
 use yii\web\NotFoundHttpException;
+use Yii;
 
 class InfoController extends Controller
 {
@@ -38,8 +40,31 @@ class InfoController extends Controller
 
     public function actionView($id)
     {
+        $model = new DateForm();
         $cart_id = OrdersHasCart::find()->asArray()->where('order_id='.$id)->one()['cart_id'];
         $profile_id = Cart::find()->asArray()->where('id='.$cart_id)->one()['profile_id'];
+
+        if($model->load(Yii::$app->request->post()))
+        {
+            $summ = OrdersHasCart::sumWithDate($profile_id,$model);
+            $query = OrdersHasCart::find()
+                ->innerJoin('orders', 'orders.id=order_id')
+                ->innerJoin('cart', 'cart.id=cart_id')
+                ->innerJoin('profile', 'profile.user_id=cart.profile_id')
+                ->innerJoin('orders_status', 'orders_status.id=orders.delivery_status')
+                ->innerJoin('store', 'store.id=cart.id_store')
+                ->where("cart.profile_id=".$profile_id." and orders.created_at BETWEEN '".$model['doDate']."' and '".$model['toDate']."'");
+            $dataProvider = new ActiveDataProvider([
+                'query' => $query
+            ]);
+            return $this->render('view',[
+                'dataProvider' => $dataProvider,
+                'summ' => $summ,
+                'model' => $model
+            ]);
+        }
+
+        $summ = OrdersHasCart::sum($profile_id);
         $query = OrdersHasCart::find()
             ->innerJoin('orders', 'orders.id=order_id')
             ->innerJoin('cart', 'cart.id=cart_id')
@@ -47,13 +72,14 @@ class InfoController extends Controller
             ->innerJoin('orders_status', 'orders_status.id=orders.delivery_status')
             ->innerJoin('store', 'store.id=cart.id_store')
             ->where('cart.profile_id='.$profile_id);
-        $profile_name = Profile::find()->asArray()->where('user_id='.$profile_id)->one()['name'];
         $dataProvider = new ActiveDataProvider([
             'query' => $query
         ]);
+
         return $this->render('view',[
             'dataProvider' => $dataProvider,
-
+            'summ' => $summ,
+            'model' => $model
         ]);
     }
 }

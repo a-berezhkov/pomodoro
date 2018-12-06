136359166043491a35934c5f782212f869312824
Andrei V. Berezhkov <dead0343@gmail.com>
2018-12-06 10:22:06 +0300

    Название сайта, убрана строка посика из админки

--- config/web.php
+++ config/web.php
@@ -5,7 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
-    'name' => 'Сеньор Помидор',
+    'name' => 'сИньор Помидор',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     // set target language to be Russian
